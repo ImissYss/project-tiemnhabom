7ee628da5fa78667b8f39b4e347c9f10e0bcc668
maitiendat <dathust165955@gmail.com>
2022-06-14 14:14:46 +0700

    tiem nha bom config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBeL-49L_SUUaoH-PJthpPyYymDFnfSaBo",
-    authDomain: "tiemnhabom-5108f.firebaseapp.com",
-    projectId: "tiemnhabom-5108f",
-    storageBucket: "tiemnhabom-5108f.appspot.com",
-    messagingSenderId: "971489261695",
-    appId: "1:971489261695:web:ab8e0831a2531d28fca74a",
-    measurementId: "G-N57BS53PNR"
-  }
+  // config firebase
 };
 
 /*
